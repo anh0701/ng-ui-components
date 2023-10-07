736fc826964218739b5a9354eeb8732d0bfce712
Pham Thi Quynh Anh <quynhanhphamthi01@gmail.com>
2023-10-08 00:14:33 +0700

    html error

--- projects/mun-tree-view/src/lib/mun-tree-view.component.ts
+++ projects/mun-tree-view/src/lib/mun-tree-view.component.ts
@@ -1,11 +1,14 @@
 import { Component } from '@angular/core';
+import { MunTreeViewService } from './mun-tree-view.service';
 
 @Component({
   selector: 'lib-mun-tree-view',
   templateUrl: './mun-tree-view.component.html',
   styles: [
-  ]
+  ],
+  providers:  [ MunTreeViewService ]
+
 })
 export class MunTreeViewComponent {
-
+  constructor(private service: MunTreeViewService){};
 }
